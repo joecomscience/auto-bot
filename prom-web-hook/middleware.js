674ec0fc8c301c624f1061a4d552a62eba2cab18
joecomscience <assassin.joe@hotmail.com>
2019-11-14 13:30:38 +0700

    add middler print body log

--- prom-web-hook/middleware.js
+++ prom-web-hook/middleware.js
@@ -1,10 +1,15 @@
-
 const errHandler = (err, req, res, next) => {
-  res.statusCode = 500
-  res.json({ error: err.message })
-  next()
+  res.statusCode = 500;
+  res.json({ error: err.message });
+  next();
+};
+
+const showSMSBody = (req, res, next) => {
+  logger.info(`sms body: ${JSON.stringify(req.body)}`);
+  next(req, res);
 };
 
 module.exports = {
   errHandler,
+  showSMSBody,
 };
