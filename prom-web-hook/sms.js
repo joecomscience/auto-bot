30f2f32523b777c3af7fd94342c4d9864333326c
joecomscience <assassin.joe@hotmail.com>
2019-11-14 13:31:15 +0700

    change lib logging

--- prom-web-hook/sms.js
+++ prom-web-hook/sms.js
@@ -1,10 +1,10 @@
-const request = require('request');
-const { parseString } = require('xml2js')
+const request = require("request");
+const { parseString } = require("xml2js");
 
 const URL = process.env.SMS_URI;
 
-const getData = (info) => {
-    return `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://services.ge.com" xmlns:bean="http://bean.ge.com">
+const getData = info => {
+  return `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://services.ge.com" xmlns:bean="http://bean.ge.com">
         <soapenv:Header/>
         <soapenv:Body>
         <ser:saveSMS>
@@ -25,85 +25,86 @@ const getData = (info) => {
         </ser:saveSMS>
         </soapenv:Body>
     </soapenv:Envelope>`;
-}
+};
 
 const sendMsgToSmsGateway = ({ message, sendTo }) => {
-    console.info(`sending message: "${message}" to: ${sendTo}`);
-    return new Promise((resolve, reject) => {
-        const body = getData({ message, sendTo });
-        const options = {
-            url: URL,
-            method: 'POST',
-            body,
-            headers: {
-                'Content-Type': 'text/xml;charset=utf-8',
-                'Content-Length': body.length,
-                'SOAPAction': 'saveSMS'
-            }
-        };
+  logger.info(`sending message: "${message}" to: ${sendTo}`);
+  return new Promise((resolve, reject) => {
+    const body = getData({ message, sendTo });
+    const options = {
+      url: URL,
+      method: "POST",
+      body,
+      headers: {
+        "Content-Type": "text/xml;charset=utf-8",
+        "Content-Length": body.length,
+        SOAPAction: "saveSMS"
+      }
+    };
 
-        request(options, (err, res, body) => {
-            if (err) {
-                console.error(err);
-                return reject(err);
-            };
-            const parseXMLToString = (err, result) => {
-                if (err) {
-                    console.error(err);
-                    return reject(err);
-                };
-                return resolve(result)
-            };
-            parseString(body, parseXMLToString)
-        });
+    request(options, (err, res, body) => {
+      if (err) {
+        logger.error(err);
+        return reject(err);
+      }
+      const parseXMLToString = (err, result) => {
+        if (err) {
+          logger.error(err);
+          return reject(err);
+        }
+        return resolve(result);
+      };
+      parseString(body, parseXMLToString);
     });
-}
+  });
+};
 
-const getPhoneNumber = () => ['0883105138'];
+const getPhoneNumber = () => ["0883105138"];
 
 const getAlertsMessage = ({ alerts }) => {
-    const msg = [];
-    for (const item of alerts) {
-        const message = item.annotations.description;
-        msg.push(message);
-    };
-    return msg;
-}
+  const msg = [];
+  for (const item of alerts) {
+    const message = item.annotations.description;
+    msg.push(message);
+  }
+  return msg;
+};
 
 const getAlertInfomation = (phones, messages) => {
-    const info = {};
-    for (const msg of messages) {
-        for (const phone of phones) {
-            info[msg] = phone;
-        };
-    };
-    return info;
-}
-
-const startSendingSMS = (information) => {
-    const requests = [];
-    for (const key in information) {
-        const message = key;
-        const sendTo = information[key];
-        requests.push(sendMsgToSmsGateway({ message, sendTo }));
+  const info = {};
+  for (const msg of messages) {
+    for (const phone of phones) {
+      info[msg] = phone;
     }
+  }
+  return info;
+};
 
-    return Promise.all(requests);
-}
+const startSendingSMS = information => {
+  const requests = [];
+  for (const key in information) {
+    const message = key;
+    const sendTo = information[key];
+    requests.push(sendMsgToSmsGateway({ message, sendTo }));
+  }
+
+  return Promise.all(requests);
+};
 
 const sendSms = async (req, res) => {
-    const phones = getPhoneNumber();
-    const alertMessages = getAlertsMessage(req.body);
-    const information = getAlertInfomation(phones, alertMessages);
+  logger.debug(req.body);
+  const phones = getPhoneNumber();
+  const alertMessages = getAlertsMessage(req.body);
+  const information = getAlertInfomation(phones, alertMessages);
 
-    try {
-        await startSendingSMS(information);
-    } catch (error) {
-        console.error(error);
-    }
-    return res.sendStatus(200)
+  try {
+    await startSendingSMS(information);
+  } catch (error) {
+    logger.error(error);
+  }
+  return res.sendStatus(200);
 };
 
 module.exports = {
-    sendSms,
+  sendSms
 };
