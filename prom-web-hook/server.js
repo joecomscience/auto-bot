77280acbf48193389c2c717beb7687f6161807a8
joecomscience <assassin.joe@hotmail.com>
2019-11-14 13:30:55 +0700

    load logging module

--- prom-web-hook/server.js
+++ prom-web-hook/server.js
@@ -1,40 +1,39 @@
-const express = require('express');
-const apiMetrics = require('prometheus-api-metrics');
-const { sendSms } = require('./sms');
-const { errHandler } = require('./middleware');
-const {
-    livediness,
-    readiness
- } = require('./healtcheck');
+const express = require("express");
+const apiMetrics = require("prometheus-api-metrics");
+const { sendSms } = require("./sms");
+const { errHandler, showSMSBody } = require("./middleware");
+const { livediness, readiness } = require("./healtcheck");
+const logger = require("./logging");
 
 const app = express();
 const port = process.env.SMS_URI ? process.env.SMS_URI : 3000;
-const onListen = () => console.log(`server start listening port: ${port}`);
+const onListen = () => logger.info(`server start listening port: ${port}`);
 
 global.ready = true;
+global.logger = logger;
 
 const gracefulShutdown = () => {
-    global.ready = false;
-    const greacefulStop = () => process.exit(0);
+  global.ready = false;
+  const greacefulStop = () => process.exit(0);
 
-    server.close((err) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        console.info('Terminating services');
-        setTimeout(greacefulStop, 2000);
-    });
-}
+  server.close(err => {
+    if (err) {
+      logger.error(err);
+      process.exit(1);
+    }
+    logger.info("Terminating services");
+    setTimeout(greacefulStop, 2000);
+  });
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(errHandler);
 app.use(apiMetrics());
-app.get('/readiness', readiness);
-app.get('/livediness', livediness);
-app.post('/sms', sendSms);
+app.get("/readiness", readiness);
+app.get("/livediness", livediness);
+app.post("/sms", showSMSBody, sendSms);
 
 const server = app.listen(port, onListen);
 
-process.on('SIGTERM', gracefulShutdown);
+process.on("SIGTERM", gracefulShutdown);
